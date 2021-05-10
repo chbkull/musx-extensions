41334aebcf64f67296b9dbe95278c90c9d074322
Christopher Kull <chkull2@illinois.edu>
2021-05-09 21:37:52 -0500

    Restructured utils files, added documentation.

--- custom_utils.py
+++ custom_utils.py
@@ -2,41 +2,74 @@ import musx
 import rtmidi
 import rtmidi.midiconstants
 
+
+# ------------------- #
+# MIDI Port Functions # 
+# ------------------- #
+
 def panic(port):
+    """Severs a MIDI connection, in case of emergency.
+    Sends note-off and control change reset messages on all channels.
+    Closes down the MIDI port.
+
+    Arguments:
+        port: rtmidi MidiOut object, port to be closed down 
+    """
     for channel in range(16):
         port.send_message([rtmidi.midiconstants.CONTROL_CHANGE, rtmidi.midiconstants.ALL_SOUND_OFF, 0])
         port.send_message([rtmidi.midiconstants.CONTROL_CHANGE, rtmidi.midiconstants.RESET_ALL_CONTROLLERS, 0])
     port.close_port()
 
-def cc_linear(queue, chan, ctrl, *, length, start, end, low=0, high=1, grain=0.05):
-    start_rescale = musx.rescale(start, low, high, 0, 127)
-    end_rescale = musx.rescale(end, low, high, 0, 127)
-    step = (end_rescale - start_rescale) / (length / grain)
-    val = start_rescale
-    for _ in musx.frange(0, length + grain, grain):
-        queue.out.addevent(musx.MidiEvent.control_change(chan, ctrl, round(val), time=queue.now))
-        val += step
-        yield grain
 
 def midi_restart(port, portname):
+    """Closes a MIDI port and opens another.
+    
+    Arguments:
+        port: rtmidi MidiOut object, port to be closed down
+        portname: string, name of MIDI port to reopen
+
+    Returns:
+        rtmidi MidiOut object, the opened MIDI port.
+    """
     panic(port)
     newport = rtmidi.MidiOut()
     newport.open_port(newport.get_ports().index(portname))
     return newport
 
 
-def bjorklund(events, steps):
+# ---------------- #
+# Helper Functions #
+# ---------------- #
+
+def bjorklund(events, positions):
+    """Algorithm for spacing out events as evenly as possible.
+    Euclidean-like algorithms are useful for producing rhythms found in world music.
+    The most interesting results are when the two arguments have a GCD of 1.
+    For example, the Cuban tresillo [x . . x . . x .] can be produced from bjorklund(3, 8), which is
+    three events spaced out in an eight positions as evenly as possible.
+    See http://cgm.cs.mcgill.ca/~godfried/publications/banff.pdf for more details.
+    
+    Arguments:
+        events: int, number of active events to place
+        positions: int, total number of positions that events can be placed
+
+    Returns:
+        A list of ones and zeros where ones denote an event and zeros are eventless.
+
+    Raises:
+        ValueError: There are either zero events or more events than positions.
     """
-    This is a test docstring.
-    """
-    if events > steps:
-        raise ValueError("Number of events cannot exceed number of steps")
+    if events > positions:
+        raise ValueError("Number of events cannot exceed number of positions")
+    if events == 0:
+        raise ValueError("Cannot have zero events")
 
     sequences = ([[1] for _ in range(events)])
-    remainder = ([[0] for _ in range(steps - events)])
+    remainder = ([[0] for _ in range(positions - events)])
 
     while len(remainder) > 0:
         diff = len(sequences) - len(remainder)
+
         for i in range(min(len(sequences), len(remainder))):
             sequences[i] += remainder.pop()
 
@@ -46,3 +79,63 @@ def bjorklund(events, steps):
     
     # List flattening comprehension from here: https://stackoverflow.com/questions/11264684/flatten-list-of-lists
     return [val for sublist in sequences for val in sublist]
+
+
+# ------------------------- #
+# Control Change Generators #
+# ------------------------- #
+
+def cc_linear(queue, *, chan, ctrl, length, start, end, low=0, high=1, grain=0.05):
+    """Changes a control change value linearly over time.
+    Remaps the start/end from low/high to 0-127, then evenly spaces out the changes according to a grain resolution.
+    
+    Arguments:
+        queue: musx Scheduler object, scheduler to add events to
+        chan: int (0-indexed), MIDI channel to send control change messages to
+        ctrl: int (0-indexed), control change value
+        length: number, total number of seconds for shift to take
+        start: number, starting value
+        end: number, ending value
+        low: number, minimum value according to start/end's scale
+        high: number, maximum value according to start/end's scale
+        grain: number, timestep between control change messages
+
+    Yields:
+        number, timestep until new control change message should be sent
+    """
+    start_rescale = musx.rescale(start, low, high, 0, 127)
+    end_rescale = musx.rescale(end, low, high, 0, 127)
+    step = (end_rescale - start_rescale) / (length / grain)
+    val = start_rescale
+    for _ in musx.frange(0, length + grain, grain):
+        queue.out.addevent(musx.MidiEvent.control_change(chan, ctrl, round(val), time=queue.now))
+        val += step
+        yield grain
+
+
+# --------------- #
+# Note Generators #
+# --------------- #
+
+def distribution(queue, *, chan, length, notes, dur, distribution=musx.uniran, low=0, high=1):
+    """Picks notes from a list according to distribution function.
+    Generates a number from a distribution function, then remaps the value from low/high to a list index.
+
+    Arguments:
+        queue: musx Scheduler object, scheduler to add events to
+        chan: int (0-indexed), MIDI channel to send note messages to
+        length: number, total number of seconds to generate messages for
+        notes: list of ints, list of MIDI note numbers to pick from
+        dur: number, duration of each note in seconds
+        distribution: function, function whose returned number dictates the next note
+        low: number, minimum value according to distribution's scale
+        high: number, maximum value according to distribution's scale
+
+    Yields:
+        number, timestep until next note should be selected and sent
+    """
+    start_time = queue.now
+    while queue.now - start_time < length:
+        selected = round(musx.rescale(distribution(), low, high, 0, len(notes) - 1))
+        queue.out.addevent(musx.MidiNote(time=queue.now, dur=dur, key=notes[selected], chan=chan))
+        yield dur
