4f9b72ba368d2c29a9fabeee5875de06a52f0c57
Christopher Kull <chkull2@illinois.edu>
2021-05-09 23:04:14 -0500

    Moving functions out of notebook.

--- custom_utils.py
+++ custom_utils.py
@@ -113,6 +113,31 @@ def cc_linear(queue, *, chan, ctrl, length, start, end, low=0, high=1, grain=0.0
         yield grain
 
 
+def cc_distribution(queue, *, chan, ctrl, length, rate, distribution=musx.uniran, low=0, high=1):
+    """Changes a control change value according to a distribution.
+    Generates a number from the distribution function, then remaps the value from low/high to 0/127.
+    
+    Arguments:
+        queue: musx Scheduler object, scheduler to add events to
+        chan: int (0-indexed), MIDI channel to send control change messages to
+        ctrl: int (0-indexed), control change value
+        length: number, total number of seconds active
+        rate: number, how frequently the control change value should be generated in seconds
+        distribution: function, function whose returned number dictates the next control change value
+        low: number, minimum value according to distribution's scale
+        high: number, maximum value according to distribution's scale
+
+    Yields:
+        number, timestep until new control change message should be sent
+    """
+    start_time = queue.now
+    while queue.now - start_time < length:
+        original = max(low, min(distribution(), high))
+        val = int(musx.rescale(original, low, high, 0, 127))
+        queue.out.addevent(musx.MidiEvent.control_change(chan, ctrl, val, time=queue.now))
+        yield rate
+
+
 # --------------- #
 # Note Generators #
 # --------------- #
@@ -138,4 +163,71 @@ def distribution(queue, *, chan, length, notes, dur, distribution=musx.uniran, l
     while queue.now - start_time < length:
         selected = round(musx.rescale(distribution(), low, high, 0, len(notes) - 1))
         queue.out.addevent(musx.MidiNote(time=queue.now, dur=dur, key=notes[selected], chan=chan))
+        yield dur
+
+
+def euclidean_rhythm(queue, *, chan, length, keynum, events, positions, cycletime, rotation=0):
+    """Plays a Euclidean rhythm.
+    Uses Bjorkland's algorithm to space out events between positions evenly, then plays it.
+
+    Arguments:
+        queue: musx Scheduler object, scheduler to add events to
+        chan: int (0-indexed), MIDI channel to send note messages to
+        length: number, total number of seconds to generate messages for
+        keynum: int, MIDI note number to send
+        events: int, number of active events to place
+        positions: int, total number of positions that events can be placed 
+        cycletime: number, number of seconds a cycle should take
+        rotation: int, position within the rhythm to start at
+        
+    Yields:
+        number, timestep until next note message should be sent
+    """
+    sequence = bjorklund(events, positions)
+    start_time = queue.now
+    notelen = cycletime / positions
+    i = rotation
+    while queue.now - start_time < length:
+        if sequence[i] == 1:
+            queue.out.addevent(musx.MidiNote(time=queue.now, dur=notelen, key=keynum, chan=chan))
+        i += 1
+        i %= positions
+        yield notelen
+
+
+def random_ascend(queue, *, chan, length, notes, dur):
+    """Plays a list of notes in an upward index trending random pattern.
+    First third weights lower, second weights middle, third weights higher.
+    Notes can spawn both in series and in parallel on each call.
+
+    Arguments:
+        queue: musx Scheduler object, scheduler to add events to
+        chan: int (0-indexed), MIDI channel to send note messages to
+        length: number, total number of seconds to generate messages for
+        notes: list of ints, list of MIDI note numbers to pick from
+        dur: number, duration of each note in seconds
+        
+    Yields:
+        number, timestep until next note message should be sent
+    """
+    rhythms = musx.choose([dur, [0, dur/2], [0, dur/4, dur/2, 3 * (dur/4)], [0, 3 * (dur/4)], [0, 0], [0, 0, dur/2, dur/2], [0, 0, 0]]) # repeated ones are a hack for triads
+    start_time = queue.now
+    while queue.now - start_time < length:
+        if queue.now - start_time < length * 0.33:
+            ran_gen = musx.lowran
+        elif queue.now - start_time < length * 0.66:
+            ran_gen = musx.midran
+        else:
+            ran_gen = musx.highran
+
+        r = next(rhythms)
+        if musx.isnum(r):
+            n = int(musx.rescale(ran_gen(), 0, 1, 0, len(notes)))
+            m = musx.MidiNote(time=queue.now, dur=dur, key=notes[n], chan=chan)
+            queue.out.addevent(m)
+        else:
+            for time in r:
+                n = int(musx.rescale(ran_gen(), 0, 1, 0, len(notes)))
+                m = musx.MidiNote(time=queue.now + time, dur=dur, key=notes[n], chan=chan)
+                queue.out.addevent(m)
         yield dur
